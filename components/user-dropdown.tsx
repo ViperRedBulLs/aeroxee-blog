1d261a5ed36c677f15e38dfcaa7aef5391e09b88
Aeroxee <vipercode305@gmail.com>
2024-03-07 17:56:53 +0700

    if click close user dropdown.

--- components/user-dropdown.tsx
+++ components/user-dropdown.tsx
@@ -11,8 +11,14 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { deleteCookie } from "cookies-next";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 
 export default function UserDropdown() {
+  const [show, setShow] = useState<boolean>(false);
+
+  const router = useRouter();
+
   // handle logout user
   const handleLogout = () => {
     deleteCookie("isLogined");
@@ -20,19 +26,37 @@ export default function UserDropdown() {
     window.location.href = "/login";
   };
 
+  const ref = useRef<any>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    document.documentElement.addEventListener("click", (e) => {
+      if (!ref.current?.contains(e.target as Element)) {
+        setShow(false);
+      }
+    });
+  });
+
   return (
-    <DropdownMenu>
+    <DropdownMenu open={show}>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" onClick={() => setShow(true)} size="icon">
           <User className="h-[1.2rem] w-[1.2rem]" />
           <span className="sr-only">User dropdown</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" ref={ref}>
         <DropdownMenuItem>
-          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/dashboard" onClick={() => setShow(false)}>
+            Dashboard
+          </Link>
         </DropdownMenuItem>
-        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href="/dashboard/settings" onClick={() => setShow(false)}>
+            Settings
+          </Link>
+        </DropdownMenuItem>
         <DropdownMenuItem>
           <button type="button" onClick={handleLogout}>
             Logout
